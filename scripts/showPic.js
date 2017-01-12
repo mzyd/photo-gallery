e4220c4480381e2a81d0bcea8da4abfc59aa982e
Mzy <mzy3991@gmail.com>
2017-01-12 18:56:26 +0800

    use nodeValue property and firstChild property to complete project, added css file

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,8 +1,19 @@
 function showPic(whichPic) {
 
-    console.log("---------");
     var source = whichPic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
-
     placeholder.setAttribute("src", source);
+
+    var text = whichPic.getAttribute("title");
+    var desc = document.getElementById("description");
+    desc.firstChild.nodeValue = text;
+
 }
+
+// function countBodyChildren() {
+//     // 这里返回的数组里面是 body 且只有一个 body, 所以需要取第0个元素
+//     var bodyElement = document.getElementsByTagName("body")[0];
+//     console.log("--------->", bodyElement.childNodes.length);
+// }
+
+// window.onload = countBodyChildren;
