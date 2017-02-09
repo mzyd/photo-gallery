f79d2477b4405b3f637a48fb3ac9e8f596306fb6
Mzy <mzy3991@gmail.com>
2017-02-09 21:50:00 +0800

    complete chapter 7.3

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,19 +1,77 @@
-function showPic(whichPic) {
-
-    var source = whichPic.getAttribute("href");
-    var placeholder = document.getElementById("placeholder");
-    placeholder.setAttribute("src", source);
-
-    var text = whichPic.getAttribute("title");
-    var desc = document.getElementById("description");
-    desc.firstChild.nodeValue = text;
-
+// 通用型函数
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        };
+    }
 }
 
-// function countBodyChildren() {
-//     // 这里返回的数组里面是 body 且只有一个 body, 所以需要取第0个元素
-//     var bodyElement = document.getElementsByTagName("body")[0];
-//     console.log("--------->", bodyElement.childNodes.length);
-// }
+// 通用型函数
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
 
-// window.onload = countBodyChildren;
+function preparePlaceholder() {
+    if (!document.createElement || !document.createTextNode || !document.getElementById) return false;
+    if (!document.getElementById("image_gallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/1.jpg");
+    placeholder.setAttribute("alt", "my image gallery");
+    var desc = document.createElement("p");
+    desc.setAttribute("id", "description");
+    var txt = document.createTextNode("Choose an image");
+    desc.appendChild(txt);
+
+    var gallery = document.getElementById("image_gallery");
+    insertAfter(placeholder, gallery);
+    insertAfter(desc, placeholder);
+}
+
+function prepareGallery() {
+    // if not supports this two methods , end it.
+    if (!document.getElementsByTagName || !document.getElementById) return false;
+    if (!document.getElementById("image_gallery")) return false;
+    var gallery = document.getElementById("image_gallery");
+    var links = gallery.getElementsByTagName("a");
+
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function() {
+            return showPic(this) ? false : true;
+        };
+        // links[i].onkeypress = links[i].onclick;
+    }
+}
+
+function showPic(whichPic) {
+    if (!document.getElementById("placeholder")) return true;
+    var source = whichPic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+
+    if (placeholder.nodeName != "IMG")  return false;
+    placeholder.setAttribute("src", source);
+
+    if (document.getElementById("description")) {
+        var text = whichPic.getAttribute("title") ? whichPic.getAttribute("title") : "";
+        var desc = document.getElementById("description");
+        if (desc.firstChild.nodeValue == 3) {
+            desc.firstChild.nodeValue = text;
+        }
+    }
+    return true;
+}
+
+
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
